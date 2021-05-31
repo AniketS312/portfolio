25d752a1c088a38de470d45384819e26b2ef891f
Aniket Sharma <aniketsharma312@gmail.com>
2021-05-31 11:49:34 -0600

    Finish up functionality on portfolio section, need to make contact section. Than finalize project

---
+++ app.js
@@ -0,0 +1,31 @@
+// Declare Variables
+const generalInfo = document.querySelectorAll('general-info');
+const dropDown = document.getElementById('dropdown');
+const moreInfo = document.getElementById('more-info');
+const icon = document.getElementById('dropdown-icon')
+
+
+
+
+
+
+
+// accodian Function
+// dropDown.addEventListener('click', function(e) {
+
+//   e.preventDefault();
+//   dropDown.style.transform = 'rotateY(180deg)'
+//   if (generalInfo.style.borderBottom = '1px solid $color-grey-custom') {
+//     generalInfo.style.borderBottom = 'none'
+//   } else {
+//     generalInfo.style.borderBottom = '1px solid $color-grey-custom'
+//   }
+//   moreInfo.classList.toggle('show')
+//   console.log('clicked')
+// })
+
+// function closeAll() {
+//   dropDown.style.transform = 'rotateY(180deg)'
+//   moreInfo.classList.remove('show')
+//   generalInfo.style.borderBottom = '1px solid $color-grey-custom'
+// }
